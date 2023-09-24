ca62c8e75be253f7d3e1bfb487b6f3733247f4ef
bol <bol@kalibol.shakur>
2023-09-24 21:28:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+// Define the currentUser variable with your name or any other name.
+const currentUser = 'benjamin bol';
+
+// Define the welcomeMessage variable using string interpolation.
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Define the excitedWelcomeMessage variable using the toUpperCase() method.
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Define the shortGreeting variable using string interpolation and .slice(0, 1).
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
